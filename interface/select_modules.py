658e3f037ee5b6c873b4a2eac471bb0a8b74fbb5
mBarneto <4347791+m-barneto@users.noreply.github.com>
2024-02-29 08:56:32 -0500

    Added OCCT module to module selector gui

--- interface/select_modules.py
+++ interface/select_modules.py
@@ -8,6 +8,7 @@ from modules.magician import Magician
 from modules.msicenter import MsiCenter
 from modules.nvidia import NVIDIA
 from modules.nzxt import NZXT
+from modules.occt import OCCT
 from modules.powerplan import PowerPlan
 from modules.superposition import Superposition
 from modules.sysprep import SysPrep
@@ -26,7 +27,7 @@ class SelectModules:
         self.modules = {}
         self.window = tk.Tk()
         self.window.title("PicoProof Installer")
-        self.window.geometry("370x600")
+        self.window.geometry("370x650")
         self.window.tk.call('tk', 'scaling', 2.0)
         self.window.config(bg="#303030")
 
@@ -74,17 +75,20 @@ class SelectModules:
         chk_superposition = CheckboxImage(Superposition(), self.window, text="Superposition")
         chk_superposition.grid(row=7, sticky='w')
         self.modules[chk_superposition['text']] = chk_superposition
+        chk_occt = CheckboxImage(OCCT(), self.window, text="OCCT")
+        chk_occt.grid(row=8, sticky='w')
+        self.modules[chk_occt['text']] = chk_occt
         chk_icue = CheckboxImage(ICue(), self.window, text="ICue")
-        chk_icue.grid(row=8, sticky='w')
+        chk_icue.grid(row=9, sticky='w')
         self.modules[chk_icue['text']] = chk_icue
         chk_nzxt = CheckboxImage(NZXT(), self.window, text="NZXT")
-        chk_nzxt.grid(row=9, sticky='w')
+        chk_nzxt.grid(row=10, sticky='w')
         self.modules[chk_nzxt['text']] = chk_nzxt
         chk_lconnect = CheckboxImage(LConnect(), self.window, text="LConnect")
-        chk_lconnect.grid(row=10, sticky='w')
+        chk_lconnect.grid(row=11, sticky='w')
         self.modules[chk_lconnect['text']] = chk_lconnect
         chk_magician = CheckboxImage(Magician(), self.window, text="Samsung Magician")
-        chk_magician.grid(row=11, sticky='w')
+        chk_magician.grid(row=12, sticky='w')
         self.modules[chk_magician['text']] = chk_magician
 
         lbl_nvidia_versions = tk.OptionMenu(self.window, self.selected_nvidia_version, *get_nvidia_versions())
@@ -100,12 +104,12 @@ class SelectModules:
         self.btn_uncheck_all.grid(row=1, column=1, sticky='e')
 
         self.btn_sysprep = tk.Button(self.window, text="Sysprep", justify="right", anchor="e", relief="groove",
-                                     command=self.sysprep)
+                                     command=SysPrep().run)
         self.btn_sysprep.grid(row=7, column=1, sticky='e')
 
         self.btn_install = tk.Button(self.window, text="   Install     ", justify="right", anchor="e", relief="groove",
                                      command=self.install)
-        self.btn_install.grid(row=11, column=1, sticky='e')
+        self.btn_install.grid(row=12, column=1, sticky='e')
 
     def set_recommended(self):
         logging.debug("Setting recommended")
@@ -113,6 +117,7 @@ class SelectModules:
 
         self.modules['Wallpaper'].update_image(True)
         self.modules['Superposition'].update_image(True)
+        self.modules['OCCT'].update_image(True)
         # check if amd maybe?
 
         self.modules['GameMode'].update_image(True)
